0174c67d88f7a4ca482f9515859c50d48fcee4b8
Timothy Goh <timothy.gcy@outlook.com>
2025-09-13 11:19:39 +0800

    AoC 2015 Day 10

---
+++ src/main/java/com/bleckshiba/y2015/Day10.java
@@ -0,0 +1,55 @@
+package com.bleckshiba.y2015;
+
+import com.bleckshiba.FileUtil;
+import com.bleckshiba.Solution;
+
+public class Day10 extends Solution<String> {
+
+  private final String data;
+
+  private final int repetition;
+
+  public Day10(String data, int repetition) {
+    this.data = data;
+    this.repetition = repetition;
+  }
+
+  public static void main(String[] args) {
+    Day10 solution = new Day10("1321131112", 50);
+    final String part1  = solution.solvePart1();
+    System.out.println(part1.length());
+  }
+
+  @Override
+  public String solvePart1() {
+    String answer = data;
+    for (int i = 1; i <= repetition; i++) {
+      answer = read(answer);
+    }
+    return answer;
+  }
+
+  @Override
+  public String solvePart2() {
+    return "";
+  }
+
+  private String read(String input) {
+    char prev = input.charAt(0);
+    int count = 0;
+    StringBuilder answer = new StringBuilder();
+    for (int i = 0; i < input.length(); i++) {
+      Character c = input.charAt(i);
+      if (c.equals(prev)) {
+        count++;
+      } else {
+        answer.append(String.format("%d%c", count, prev));
+        count = 1;
+        prev = c;
+      }
+    }
+    answer.append(String.format("%d%c", count, prev));
+
+    return answer.toString();
+  }
+}
